f94411033051c31ecb14bb2c91e86ef7233dc70d
Lara Clemens <lclemens@uci.edu>
2019-02-20 14:48:48 -0800

    Add script to copy concatenate output files into CatFiles folder. Add make command copycat.

---
+++ src/HPC/copyCatFiles.sh
@@ -0,0 +1,9 @@
+##############################################
+# Copy and move cat files
+# 20 02 2019
+
+cp CatFiles/*.cat ../CatFiles/
+
+cp MissingFiles.$ITER ../CatFiles/MissingFiles/
+
+echo "Finished copying cat files."
